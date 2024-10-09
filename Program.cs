bf9138a5f807f877887a07ba1bc1eba6c9f0508d
Nathan Barnes <339723215@GoTVDSB.ca>
2024-10-09 08:39:29 -0400

    3tyu

--- Program.cs
+++ Program.cs
@@ -1,10 +1,39 @@
-﻿namespace T6_random_numbers
+﻿using System.Runtime.CompilerServices;
+
+namespace T6_random_numbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Random generator  = new Random();
+            //int randNum;
+            //randNum = generator.Next(10);
+           
+
+            //Console.WriteLine("My randum number is " + randNum);
+            Random generator = new Random();
+            int num1 = generator.Next(2);
+            int num2 = generator.Next(2);
+            Console.WriteLine(num2);
+            if (num1 == num2)
+            {
+                Console.WriteLine("Hey! the numbers are the same thats rare.");
+            }
+            else
+            {
+                Console.WriteLine("Hey, the numbers are different. Thats normal.");
+            }
+
+
+
+
         }
-    }
+    }    
+    
+
+
+
+ 
+
 }
